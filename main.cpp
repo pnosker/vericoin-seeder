213b1cbbc87c6d6e6d61c3a1f0bc1b22088ad917
pnosker <pnosker@gmail.com>
2014-05-21 21:20:39 -0400

    Added supernodes 4 and 5

--- main.cpp
+++ main.cpp
@@ -342,7 +342,7 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"supernode.vericoin.info", "supernode2.vericoin.info", "supernode3.vericoin.info", ""};
+static const string mainnet_seeds[] = {"supernode.vericoin.info", "supernode2.vericoin.info", "supernode3.vericoin.info", "supernode4.vericoin.info", "supernode5.vericoin.info", ""};
 static const string testnet_seeds[] = {"testnet-seed.vericoin.info", ""};
 static const string *seeds = mainnet_seeds;
 
